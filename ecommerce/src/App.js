c7a5c382bb64bfcb416851bafad107b7ad06a051
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-08 23:26:47 -0600

    css added for app

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Basket from './pages/Basket';
 import { StoreProvider } from './context-and-reduce/StoreContext';
 
 function App() {
   return (
-    <div className="App">
     <StoreProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/basket' element={<Basket />}/>
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/basket" element={<Basket />} />
+          </Routes>
+        </div>
+      </Router>
     </StoreProvider>
-    </div>
   );
 }
 
