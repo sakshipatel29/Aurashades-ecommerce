0f85582840812315068605a15bfeb029a7db0dd7
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-08 23:23:38 -0600

    css for basket prpducts.

--- ecommerce/src/components/BasketProduct.js
+++ ecommerce/src/components/BasketProduct.js
@@ -1,22 +1,26 @@
 import React, { useContext } from 'react';
 import { StoreContext } from '../context-and-reduce/StoreContext';
+import './BasketProduct.css'; // Import the CSS file
 
-export const BasketProduct = ({item}) => {
-    const { removeFromBasket } = useContext(StoreContext);
+export const BasketProduct = ({ item }) => {
+  const { removeFromBasket } = useContext(StoreContext);
 
   const handleRemove = () => {
     removeFromBasket(item);
-  }
-  return (
-    <div>
-        <img src={item.image} alt={item.name + "glasses"} /> 
-        <div>
-            <p>{item.name}</p>
-            <p>${item.price.toFixed(2)}</p>
-        </div>
-        <button onClick={handleRemove}>Remove</button>
-    </div>
-  )
-}
+  };
 
-export default BasketProduct
+  return (
+    <div className="basket-product">
+      <img src={item.image} alt={item.name + " glasses"} className="product-image" />
+      <div className="product-details">
+        <p className="product-name">{item.name}</p>
+        <p className="product-price">${item.price.toFixed(2)}</p>
+      </div>
+      <button className="remove-button" onClick={handleRemove}>
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default BasketProduct;
